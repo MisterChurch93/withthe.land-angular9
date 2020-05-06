acd59caf80add02cb03b73a68ba50d488a71fbee
Odyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-05-06 06:46:45 -0400

    add: implements OnInit, and headroom code for header scrolling.

--- withtheland/src/app/app.component.ts
+++ withtheland/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import Headroom from "headroom.js";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'withtheland';
+
+  ngOnInit (): void {
+    const header = document.querySelector("header");
+    const headroom = new Headroom(header);
+    headroom.init();
+  }
 }
