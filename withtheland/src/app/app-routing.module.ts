d0f56a95a6a6fb3e960ae17366c804841f105f04
tvOdyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-05-06 22:21:36 -0400

    add; welcome page to routing module

--- withtheland/src/app/app-routing.module.ts
+++ withtheland/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'welcome',
+    component: WelcomeComponent,
+    data: {
+      title: 'Welcome'
+    }
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
