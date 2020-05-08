bea508f078f9876c4e4ac3854c7e972b3df946b5
tvOdyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-05-08 05:53:29 -0400

    update; browser animation support in the future

--- withtheland/src/app/app.component.ts
+++ withtheland/src/app/app.component.ts
@@ -1,17 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+
 import Headroom from "headroom.js";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: []
 })
 export class AppComponent implements OnInit {
   title = 'withtheland';
+  private previousPath: string = ''
 
   ngOnInit (): void {
     const header = document.querySelector("header");
     const headroom = new Headroom(header);
     headroom.init();
   }
+
 }
--- withtheland/src/app/app.module.ts
+++ withtheland/src/app/app.module.ts
@@ -1,4 +1,5 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { NgModule } from '@angular/core';
 import { HashLocationStrategy, LocationStrategy  } from '@angular/common';
 
@@ -7,6 +8,8 @@ import { AppComponent } from './app.component';
 import { CompanyModule } from './pages/company/company.module';
 import { WelcomeComponent } from './pages/welcome/welcome.component';
 
+// https://source.unsplash.com/1920x1080/?canada forest,canada trees,canada mountains,canada nature
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,6 +17,7 @@ import { WelcomeComponent } from './pages/welcome/welcome.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     CompanyModule
   ],
